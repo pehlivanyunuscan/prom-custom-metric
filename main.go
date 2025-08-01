4e36e9ee126876bd943f48f01d56b6f35b135dc4
pehlivanyunuscan <pehlivanyunuscan@hotmail.com>
2025-08-01 16:02:31 +0300

    feat(metrics): /metrics endpointi ve Prometheus metric kayıtları eklendi

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"math/rand/v2"
+
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
+	// Gauge: güneş paneli çıkış gücü (panel_id ile label)
 	solarPanelGauge = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "solar_panel_output_watts",
@@ -12,7 +18,7 @@ var (
 		},
 		[]string{"panel_id"},
 	)
-
+	// Counter: /metrics endpointine yapılan istek sayısı
 	metricsCounter = prometheus.NewCounter(
 		prometheus.CounterOpts{
 			Name: "metrics_endpoint_requests_total",
@@ -20,3 +26,26 @@ var (
 		},
 	)
 )
+
+func init() {
+	// Metricleri register et
+	prometheus.MustRegister(solarPanelGauge)
+	prometheus.MustRegister(metricsCounter)
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/metrics", func(c *fiber.Ctx) error {
+		metricsCounter.Inc()
+
+		for _, panelID := range []string{"A", "B", "C", "D", "E", "F"} {
+			value := 100 + rand.Float64()*400 // Örnek değer
+			solarPanelGauge.WithLabelValues(panelID).Set(value)
+		}
+
+		return adaptor.HTTPHandler(promhttp.Handler())(c)
+	})
+
+	app.Listen(":8080")
+}
