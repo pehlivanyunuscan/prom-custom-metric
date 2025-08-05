84e7c19e622f525e8e007144cffdf64062e21222
pehlivanyunuscan <pehlivanyunuscan@hotmail.com>
2025-08-05 10:28:58 +0300

    feat(metrics): sensör label'ları ve random değer fonksiyonu eklendi

--- main.go
+++ main.go
@@ -39,6 +39,54 @@ var (
 	)
 )
 
+var sensorLabels = []string{
+	"aku gerilimi",
+	"panel gerilimi",
+	"sarj akimi",
+	"yuk akimi",
+	"sicaklik",
+	"soc",
+	"yuk gucu",
+	"panel gucu",
+	"yuk durum",
+	"aku tipi",
+	"sarj durum",
+	"kapi bilgisi",
+	"sarj gucu",
+	"panel akimi",
+}
+
+var roleLabels = []string{
+	"role 1",
+	"role 2",
+	"role 3",
+	"role 4",
+	"role 5",
+	"role 6",
+	"role 7",
+}
+
+func randomValue(sensor string) float64 {
+	// Her sensör için rastgele bir değer döndür
+	switch sensor {
+		case "aku gerilimi", "panel gerilimi":
+			return float64(rand.IntN(1500)+1200)
+		case "sarj akimi", "yuk akimi", "panel akimi":
+			return float64(rand.IntN(3000)+500)
+		case "sicaklik":
+			return float64(rand.IntN(100))
+		case "soc":
+			return float64(rand.IntN(101))
+		case "yuk gucu", "panel gucu", "sarj gucu":
+			return float64(rand.IntN(4000)+1000)
+		case "yuk durum", "aku tipi", "sarj durum",:
+			return float64(rand.IntN(2)) // 0 veya 1
+		case "kapi bilgisi":
+			return float64(rand.IntN(3)) // 0, 1 veya 2
+		default:
+			return 0.0 // Bilinmeyen sensör
+	}
+}
 func init() {
 	// Metricleri register et
 	prometheus.MustRegister(mpptGauge)
